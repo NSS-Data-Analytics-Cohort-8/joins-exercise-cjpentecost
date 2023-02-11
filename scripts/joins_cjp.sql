8dd9fbfdb3f65c4b194baacc8caba9a4ee57cdd2
chris pentecost <cjpentecost15@gmail.com>
2023-02-11 11:39:12 -0600

    finished q4

--- scripts/joins_cjp.sql
+++ scripts/joins_cjp.sql
@@ -29,6 +29,14 @@ ORDER BY worldwide_gross DESC;
 -- 4. Write a query that returns, for each distributor in the distributors table, the distributor name and the number of movies associated with that distributor in the movies 
 -- table. Your result set should include all of the distributors, whether or not they have any movies in the movies table.
 
+SELECT company_name, COUNT(film_title) AS film_title
+FROM distributors
+FULL JOIN specs
+ON distributors.distributor_id = specs.domestic_distributor_id 
+GROUP BY company_name
+ORDER BY film_title DESC;
+--There are 11 movies with NULL as the Distributor ID
+
 -- 5. Write a query that returns the five distributors with the highest average movie budget.
 
 -- 6. How many movies in the dataset are distributed by a company which is not headquartered in California? Which of these movies has the highest imdb rating?
